fbc540aefc766661926cf7d2e0533cea0c071b27
Tim <tim@tumblr.com>
2016-04-11 15:13:12 -0400

    Adding URL input to Data Lasso, cleaning up Uploader view

--- src/views/Uploader.js
+++ src/views/Uploader.js
@@ -10,78 +10,165 @@ var dispatcher = require('../dispatcher');
 /**
  * ## Upload Form View
  *
- * Contains logic of uploading files into data lasso
- * to be visualized.
- *
+ * Handles UI and logic of loading data into Data Lasso
+ * either using file or by requesting dataset from a URL
  */
 
 var UploaderView = Backbone.View.extend({
 
     className: 'uploader',
 
+    /**
+     * What source type is currently selected
+     */
+    dataSource: 'file',
+
+    /**
+     * What format is the data source
+     */
+    fileType: 'csv',
+
     events: {
-        'submit form': 'onFormSubmit',
+        'click [data-js-selector="data-source-toggle"]': 'onDataSourceClick',
+        'click [data-js-selector="file-type-toggle"]': 'onFileTypeClick',
+
+        'submit [data-js-selector="file-upload-form"]': 'onFileFormSubmit',
+        'submit [data-js-selector="url-form"]': 'onUrlFormSubmit',
     },
 
     /**
-     * Get whatever filetype was checked
+     * Event handler for data source toggle click. Sets
+     * `data-source` attribute on the $el to match selected data source
      *
-     * @returns {string} : Either 'csv', 'tsv' or 'json'
+     * @param e - DOM Event
      */
-    getSelectedType: function () {
-        var $selectedToggle = this.$formatToggles.filter(':checked');
-        return $selectedToggle.val();
+    onDataSourceClick: function (e) {
+        var source = $(e.target).attr('data-source');
+        this.$el.attr('data-source', source);
     },
 
     /**
-     * Event listener for form submit event
+     * Event handler for file type toggle click. Sets
+     * `data-file-type` attribute on the $el to match selected file type
+     *
+     * @param e - DOM Event
      */
-    onFormSubmit: function (e) {
+    onFileTypeClick: function (e) {
+        var type = $(e.target).attr('data-type');
+        this.$el.attr('data-file-type', type);
+    },
+
+    /**
+     * Event listener for file upload form submit event
+     *
+     * @param e - DOM Event from which we get reference to file input
+     */
+    onFileFormSubmit: function (e) {
         e.preventDefault();
 
-        var file = this.$fileUpload[0].files[0];
-
+        var file = e.target[0].files[0];
         if (file) {
+            this.setLoadingIndicator(true);
             var reader = new FileReader();
-            reader.onload = _.bind(this.readerOnload, this);
+            reader.onload = _.bind(function (e) {
+                this.loadData(e.target.result);
+            }, this);
             reader.readAsText(file);
-            this.toggleLoadingIndicator();
         }
     },
 
     /**
-     * Callback for when file was accepted and read. Parses file
-     * into an object and passes it further down the flow
+     * Event listener for url form submit event
+     *
+     * @param e - DOM Event from which we get URL to use
      */
-    readerOnload: function (e) {
-        var entries;
+    onUrlFormSubmit: function (e) {
+        e.preventDefault();
 
-        switch (this.getSelectedType()) {
-            case 'csv':
-                entries = d3.csv.parse(e.target.result);
-                break;
-            case 'tsv':
-                entries = d3.tsv.parse(e.target.result);
-                break;
-            case 'json':
-                entries = JSON.parse(e.target.result);
-                break;
+        var url = e.target[0].value;
+        if (url) {
+            this.setLoadingIndicator(true);
+            $.get(url)
+                .then(_.bind(this.loadData, this))
+                .fail(_.bind(this.onRequestFail, this));
         }
-
-        dispatcher.dispatch({actionType: 'file-uploaded', entries: entries});
-
-        this.listenToOnce(store, 'change:entries', this.toggleLoadingIndicator());
     },
 
-    toggleLoadingIndicator: function () {
-        this.$el.toggleClass('loading');
+    onRequestFail: function (jqXHR, status, error) {
+        this.setLoadingIndicator(false);
+        this.displayError(error);
+    },
+
+    /**
+     * Parses file into an object based on it's type.
+     * Uses d3's parsing to parse a string to an object
+     *
+     * @param {string} data - String to parse
+     * @return {object} - List of entries to visualize
+     */
+    parseFile: function (data) {
+        try {
+            switch (this.fileType) {
+                case 'csv':
+                    return d3.csv.parse(data);
+                case 'tsv':
+                    return d3.tsv.parse(data);
+                case 'json':
+                    return JSON.parse(data);
+            }
+        } catch (e) {
+            this.displayError(e);
+            return null
+        }
+    },
+
+    /**
+     * Load the data to Data Lasso. Attempts to parse
+     * uploaded or requested file first, and updates the state if succesfull.
+     *
+     * @param {string} data - String containg file that was uploaded or requested
+     */
+    loadData: function (data) {
+        if (data) {
+            var entries = this.parseFile(data);
+
+            if (entries) {
+                this.listenToOnce(store, 'change:entries', _.bind(this.setLoadingIndicator, this, false));
+                dispatcher.dispatch({actionType: 'file-uploaded', entries: entries});
+            } else {
+                this.setLoadingIndicator(false);
+            }
+        } else {
+            this.displayError();
+        }
+    },
+
+    /**
+     * Sets loading state on the Uploader component by
+     * adding or removing `loading` class
+     *
+     * @param {boolean} isLoading - Should interface be seen as loading
+     */
+    setLoadingIndicator: function (isLoading) {
+        this.$el.toggleClass('loading', isLoading);
+    },
+
+    /**
+     * Display an error state to the user by applying an
+     * animation that is added with `error` class
+     */
+    displayError: function () {
+        this.$el.addClass('error');
+        this.$el.on('animationend', _.bind(function () {
+            this.$el.removeClass('error');
+        }, this));
     },
 
     render: function () {
         this.$el.html(template());
 
-        this.$fileUpload = this.$('[type="file"]');
-        this.$formatToggles = this.$('[name="type"]');
+        this.$el.attr('data-source', this.dataSource);
+        this.$el.attr('data-file-type', this.fileType);
 
         return this;
     },
